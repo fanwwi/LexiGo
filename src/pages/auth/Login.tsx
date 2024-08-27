d74bedf741ad9d3aef81472aff189802fd0987da
fanwwi <farangiz.anvarjanova553@gmail.com>
2024-08-27 20:56:16 +0600

    .

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,21 +1,8 @@
 import React from "react";
-import styles from "./auth.module.css"
+import styles from "./auth.module.css";
 
 const Login = () => {
-  return (
-    <div>
-      <div className="top">
-        <span>Upgrade your</span>
-        <h1>Language skills</h1>
-        <p>Learn kyrgyz language in English, Russian and Hindi</p>
-      </div>
-      <img src="" alt="" />
-      <div className={styles.bottom}>
-        <button id={styles.signIn}>Sign In</button>
-        <button id={styles.sigUp}>Create an account</button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Login;
--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -2,7 +2,20 @@ import React from "react";
 import styles from "./mainPage.module.css";
 
 const MainPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="top">
+        <span>Upgrade your</span>
+        <h1>Language skills</h1>
+        <p>Learn kyrgyz language in English, Russian and Hindi</p>
+      </div>
+      <img src="" alt="" />
+      <div className={styles.bottom}>
+        <button id={styles.signIn}>Sign In</button>
+        <button id={styles.sigUp}>Create an account</button>
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
